22fd025b4f1c71c62bdae9da4d23caf66b519266
Bonjune <malloc099@gmail.com>
2020-08-26 17:39:39 +0900

    Created clock and the test client

---
+++ src/Normio.Clock.Test/Program.fs
@@ -0,0 +1,39 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+open Grpc.Core
+open Google.Protobuf.WellKnownTypes
+open Normio.Clock
+
+type ClockServiceImpl(client: ClockService.ClockServiceClient) =
+    member __.Subscribe(req: RegisterReq) =
+        let subs = client.Subscribe(req)
+        let stream = subs.ResponseStream
+        let mutable i = 0
+        while stream.MoveNext(Threading.CancellationToken.None) |> Async.AwaitTask |> Async.RunSynchronously do
+            let curr = stream.Current
+            (curr.RoomId, curr.ClockMessage)
+            |> printfn "Got a response %d %A" i
+
+            i <- i + 1
+        
+
+[<EntryPoint>]
+let main argv =
+    let channel = Channel("127.0.0.1:8080", ChannelCredentials.Insecure)
+    let client = ClockServiceImpl(ClockService.ClockServiceClient(channel))
+
+    let req () = RegisterReq(
+                    RoomId = Guid.NewGuid().ToString(),
+                    ExamStart = Timestamp.FromDateTimeOffset DateTimeOffset.Now,
+                    ExamEnd = Timestamp.FromDateTimeOffset DateTimeOffset.Now
+                )
+    
+    for _ in 1..10 do
+        client.Subscribe(req())
+        
+    Console.ReadKey() |> ignore
+
+    channel.ShutdownAsync().Wait()
+    
+    0 // return an integer exit code
--- src/Normio.Clock/Program.fs
+++ src/Normio.Clock/Program.fs
@@ -1,33 +1,121 @@
-﻿// Learn more about F# at http://fsharp.org
-
-open System
-
-type Notification = {
-    StartExam: Guid -> Async<unit>
-    EndExam: Guid -> Async<unit>
-}
+﻿open System
+open System.IO
+open System.Threading.Tasks
+open Grpc.Core
+open Normio.Clock
+open Google.Protobuf
 
 type NotiTime = System.DateTimeOffset
 
-type NotiAction = NotiTime * Notification
+type NotiAction = Guid -> NotiTime -> Async<unit>
 
-/// Maintains a minimum heap structure w.r.t `NotiAction.NotiTime`
-type NotiHeap =
-    | Node of NotiHeap * NotiAction * NotiHeap
-    | Empty
+type Notification = {
+    Id: Guid
+    StartExam: NotiTime
+    EndExam: NotiTime
+}
+let waitUntil time =
+    let isOver () = DateTimeOffset.Compare(DateTimeOffset.Now, time) >= 0
+    let rec loop () =
+        if isOver ()
+        then ()
+        else loop ()
+    loop ()
 
-module NotiHeap =
-    let rec add action heap =
-        let (time, noti) = action
-        match heap with
-        | Node (left, act, right) ->
-            if time < (fst act)
-            then Node (add act heap, action, right)
-            else Node (add action left , act, right)
-        | Empty -> Node(Empty, action, Empty)
+// type Notifier (Id: uint32) =
+//     do printfn "New notifier spawned! %u" Id
 
+//     let doStartExam (req: RegisterReq) (stream: IServerStreamWriter<RegisterRes>) =
+//         RegisterRes(RoomId = req.RoomId, ClockMessage = ClockMessage.StartExam)
+//         |> stream.WriteAsync
+//         |> Async.AwaitTask
+//         |> Async.RunSynchronously
+//         printfn "[*] The message is successfully sent..."
+
+//     let doEndExam (req: RegisterReq) (stream: IServerStreamWriter<RegisterRes>) =
+//         RegisterRes(RoomId = req.RoomId, ClockMessage = ClockMessage.EndExam)
+//         |> stream.WriteAsync
+//         |> Async.AwaitTask
+//         |> Async.RunSynchronously
+//         printfn "[*] The message is successfully sent..."
+
+//     let agent = MailboxProcessor.Start (fun (inbox: MailboxProcessor<RegisterReq * IServerStreamWriter<RegisterRes>>) ->
+//         let rec loop () = async {
+//                 let! (req, stream) = inbox.Receive()
+                
+//                 req.ExamStart.ToDateTimeOffset() |> waitUntil
+//                 printfn "[*] Sending start message..."
+//                 doStartExam req stream
+
+//                 req.ExamEnd.ToDateTimeOffset() |> waitUntil 
+//                 printfn "[*] Sending end message..."
+//                 doEndExam req stream
+
+//                 return! loop ()
+//             }
+
+//         loop ()
+//     )
+
+//     member __.Work (req, stream) = agent.Post (req, stream)
+
+
+
+// type Spawner () =
+//     let spawnNotifier req stream i =
+//         let notifier = Notifier(i)
+//         notifier.Work (req, stream)
+
+//     let agent = MailboxProcessor.Start (fun inbox -> 
+//         let mutable i = 0u
+//         let rec loop () = async {
+//             // waits for a notification to arrive
+//             let! (req, stream) = inbox.Receive () 
+//             spawnNotifier req stream i
+//             i <- i + 1u
+//             return! loop ()
+//         }
+
+//         loop ()
+//     )
+
+//     member __.Spawn req stream =
+//         printfn "Spawning new notifier %A" req 
+//         agent.Post (req, stream)
+
+type ClockServiceImpl() =
+    inherit ClockService.ClockServiceBase()
+
+    override __.Subscribe(req: RegisterReq, responseStream: IServerStreamWriter<RegisterRes>, ctx: ServerCallContext): Task =
+        fun () ->
+            // spawner.Spawn req responseStream
+            req.ExamStart.ToDateTimeOffset() |> waitUntil
+            RegisterRes(RoomId = req.RoomId, ClockMessage = ClockMessage.StartExam)
+            |> responseStream.WriteAsync
+            |> Async.AwaitTask
+            |> Async.RunSynchronously
+
+            req.ExamEnd.ToDateTimeOffset() |> waitUntil
+            RegisterRes(RoomId = req.RoomId, ClockMessage = ClockMessage.EndExam)
+            |> responseStream.WriteAsync
+            |> Async.AwaitTask
+            |> Async.RunSynchronously
+        |> Task.Run
+
+let [<Literal>] Address = "127.0.0.1"
+let [<Literal>] Port = 8080
 
 [<EntryPoint>]
 let main argv =
-    printfn "Running Normio Clock"
+    printfn "##### Normio Clock Server #####"
+    let server = Server()
+    let service = ClockServiceImpl()
+    server.Services.Add(ClockService.BindService(service))
+    server.Ports.Add(ServerPort(Address, Port, ServerCredentials.Insecure)) |> ignore
+    server.Start()
+    
+    let _ = Console.ReadKey()
+
+
+    server.ShutdownAsync().Wait()
     0 // return an integer exit code
