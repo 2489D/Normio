bb5befe01a8ee570491b5e03018ae416b1ac26b0
Bonjune <malloc099@gmail.com>
2020-09-27 00:36:29 +0900

    Cleaned deprecated modules

--- src/deprecated/Normio.Clock/Normio.Persistence.ImplTest/Library.fs
+++ src/deprecated/Normio.Clock/Normio.Persistence.ImplTest/Library.fs
@@ -1,5 +1,9 @@
 ﻿namespace Normio.Persistence.ImplTest
 
+open NEventStore
+
 module Say =
     let hello name =
+        Wireup.Init()
+            .UsingSqlPersistence("l")
         printfn "Hello %s" name
