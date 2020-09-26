c6a2406298c39fd73159c86dce0b73b81eed9e03
Bonjune <malloc099@gmail.com>
2020-09-26 23:41:21 +0900

    Add Web Server Project using Suave

---
+++ src/Normio.Web/Program.fs
@@ -0,0 +1,9 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+open Suave
+
+[<EntryPoint>]
+let main argv =
+    startWebServer defaultConfig (Successful.OK "Hello Suave!")
+    0 // return an integer exit code
