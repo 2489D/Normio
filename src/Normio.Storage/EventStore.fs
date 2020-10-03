4aaf80c0f50578875253a32e941fdc310886abea
Bonjune <malloc099@gmail.com>
2020-10-03 19:51:11 +0900

    Create toString function for event store errors

--- src/Normio.Storage/EventStore.fs
+++ src/Normio.Storage/EventStore.fs
@@ -7,6 +7,10 @@ open Normio.Events
 type EventStoreError =
     | FailToFindExam of Guid
 
+module EventStoreError =
+    let toString = function
+        | FailToFindExam examId -> sprintf "Fail to find the exam of %A" examId
+
 type Err = EventStoreError
 type EventStore = {
     GetState: Guid -> Async<Result<State, Err>>
