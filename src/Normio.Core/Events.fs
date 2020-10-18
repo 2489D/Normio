0dbeeff859aed0c2ea0196260c92a6f2c11d7c15
Bonjune <malloc099@gmail.com>
2020-10-18 21:08:53 +0900

    feature: add parameter name for event constructors

--- src/Normio.Core/Events.fs
+++ src/Normio.Core/Events.fs
@@ -4,15 +4,16 @@ open System
 open Normio.Core.Domain
 
 type Event =
-    | ExamOpened of Guid * ExamTitle40
-    | ExamStarted of Guid
-    | ExamEnded of Guid
-    | ExamClosed of Guid
-    | StudentEntered of Guid * Student
-    | StudentLeft of Guid * Guid
-    | HostEntered of Guid * Host
-    | HostLeft of Guid * Guid
-    | SubmissionCreated of Guid * Submission
-    | QuestionCreated of Guid * File
-    | QuestionDeleted of Guid * Guid
-    | TitleChanged of Guid * ExamTitle40
+    | ExamOpened of examId:Guid * title:ExamTitle40
+    | ExamStarted of examId:Guid
+    | ExamEnded of examId:Guid
+    | ExamClosed of examId:Guid
+    | StudentEntered of examId:Guid * student:Student
+    | StudentLeft of examId:Guid * studentId:Guid
+    | HostEntered of examId:Guid * host:Host
+    | HostLeft of examId:Guid * hostId:Guid
+    | SubmissionCreated of examId:Guid * submission:Submission
+    | QuestionCreated of examId:Guid * file:File
+    | QuestionDeleted of examId:Guid * fileId:Guid
+    | TitleChanged of examId:Guid * title:ExamTitle40
+    
