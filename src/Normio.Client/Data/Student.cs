529d7c7b6e494af5771195145ee9c7ff39505d03
Seungmin Hwang <hwangtmdals@kaist.ac.kr>
2020-11-09 05:05:29 +0900

    add: separate login page; host and student

---
+++ src/Normio.Client/Data/Student.cs
@@ -0,0 +1,9 @@
+namespace Normio.Client.Data
+{
+    public class Student
+    {
+        public string Name { get; set; }
+        public string StudentId { get; set; }
+        public string Password { get; set; }
+    }
+}
