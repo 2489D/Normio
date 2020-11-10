767da2ccf94e65dc49efbf9dd64411c95703d1d8
SunghyunKimKAIST <harryzzong@kaist.ac.kr>
2020-11-11 05:25:31 +0900

    add : update timer tests

--- src/Normio.Tests/Timer.Tests.fs
+++ src/Normio.Tests/Timer.Tests.fs
@@ -23,7 +23,7 @@ let ``timer should be created`` () =
     }
 
     try
-        inMemoryTimer.SetTimer (DateTime.Now.AddSeconds(float 3)) task |> ignore
+        inMemoryTimer.SetTimer (DateTime.Now.AddSeconds(float 3)) task |> Async.RunSynchronously |> ignore
     with
     | _ ->
         failwith "This should not fail"
@@ -36,48 +36,75 @@ let ``timer should not be created`` () =
         failwith "this should not be executed"
     }
 
-    (fun () -> inMemoryTimer.SetTimer (DateTime.Now.AddSeconds(float -3)) task |> ignore)
-    |> should (throwWithMessage "The given time is in the past") typeof<System.Exception>
+    let past_3sec = DateTime.Now.AddSeconds(float -3)
+
+    (fun () -> inMemoryTimer.SetTimer past_3sec task |> Async.RunSynchronously |> ignore)
+    |> should (throwWithMessage (sprintf "The given time is in the past: %A" past_3sec)) typeof<System.Exception>
 
 
 [<Fact>]
 let ``timer execute task 1`` () =
-    use inMemoryTimer = createInMemoryTimer (float 1000)
+    use inMemoryTimer = createInMemoryTimer (float 100)
     let mutable result = 0
     let task = async {
         result <- 1
     }
 
-    inMemoryTimer.SetTimer (DateTime.Now.AddSeconds(float 3)) task |> ignore
-    Threading.Thread.Sleep(4000)
+    inMemoryTimer.SetTimer (DateTime.Now.AddMilliseconds(float 300)) task |> Async.RunSynchronously |> ignore
+
+    Threading.Thread.Sleep(400)
     result |> should equal 1
 
 [<Fact>]
 let ``timer execute task 2`` () =
-    use inMemoryTimer = createInMemoryTimer (float 1000)
+    use inMemoryTimer = createInMemoryTimer (float 100)
     let mutable result = 0
     let task = async {
         result <- 1
     }
 
-    inMemoryTimer.SetTimer (DateTime.Now.AddSeconds(float 3)) task |> ignore
-    Threading.Thread.Sleep(1000)
+    inMemoryTimer.SetTimer (DateTime.Now.AddMilliseconds(float 300)) task |> Async.RunSynchronously |> ignore
+
+    Threading.Thread.Sleep(100)
     result |> should equal 0
-    Threading.Thread.Sleep(3000)
+    Threading.Thread.Sleep(300)
     result |> should equal 1
 
 [<Fact>]
 let ``timer execute task 3`` () =
-    let interval = 1000
+    let interval = 100
     use inMemoryTimer = createInMemoryTimer (float interval)
     let mutable result = 0
     let task amount = async {
         result <- result + amount
     }
 
-    inMemoryTimer.SetTimer (DateTime.Now.AddSeconds(float 1)) (task 1) |> ignore
-    inMemoryTimer.SetTimer (DateTime.Now.AddSeconds(float 2)) (task 100) |> ignore
-    Threading.Thread.Sleep(1 * 1000 + interval)
+    inMemoryTimer.SetTimer (DateTime.Now.AddMilliseconds(float 100)) (task 1) |> Async.RunSynchronously |> ignore
+    inMemoryTimer.SetTimer (DateTime.Now.AddMilliseconds(float 200)) (task 100) |> Async.RunSynchronously |> ignore
+
+    Threading.Thread.Sleep(interval + 100)
     result |> should equal 1
-    Threading.Thread.Sleep(1 * 1000)
+    Threading.Thread.Sleep(100)
+    result |> should equal 101
+
+// FIXME : concurrency problem
+// do we need MailboxProcessor in timer?
+[<Fact>]
+let ``timers should be set correctly even if more than one SetTimer called at the same time`` () =
+    let interval = 100
+    use inMemoryTimer = createInMemoryTimer (float interval)
+    let mutable result = 0
+    let task amount = async {
+        result <- result + amount
+    }
+
+    [inMemoryTimer.SetTimer (DateTime.Now.AddMilliseconds(float 100)) (task 1);
+    inMemoryTimer.SetTimer (DateTime.Now.AddMilliseconds(float 200)) (task 100)]
+    |> Async.Parallel
+    |> Async.RunSynchronously
+    |> ignore
+
+    Threading.Thread.Sleep(interval + 100)
+    result |> should equal 1    // FIXME : usually 100, sometimes 101 (why?) FUCK YOU
+    Threading.Thread.Sleep(100)
     result |> should equal 101
