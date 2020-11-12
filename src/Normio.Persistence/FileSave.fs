970dfbb481b465f5aaf521a03d05d74999c7d44c
SunghyunKimKAIST <harryzzong@kaist.ac.kr>
2020-11-13 07:45:16 +0900

    feat : file saver & getter

--- src/Normio.Persistence/FileSave.fs
+++ src/Normio.Persistence/FileSave.fs
@@ -3,8 +3,29 @@
 open System
 open System.IO
 
+// what to do if the file already exist?
+// currently: just overlap
+type IFileSaver =
+    abstract SaveQuestion: examId: Guid -> questionId: Guid -> questionStream: FileStream -> Async<unit>
+    abstract SaveSubmission: examId: Guid -> studentId: Guid -> submissionId: Guid -> submissionStream: FileStream -> Async<unit>
+    // both two can throw exception
+
 (*
-    "path"
+    3 way to design IFileGetter functions
+
+    1. get callback function in input (current way)
+    2. return async<FileStream>
+    3. return Task<byte[]> (return value from File.ReadAllBytesAsync)
+*)
+type IFileGetter =
+    abstract GetQuestion: examId: Guid -> questionId: Guid -> callback: (FileStream -> 'a) -> Async<'a>
+    abstract GetSubmission: examId: Guid -> studentId: Guid -> submissionId: Guid -> callback: (FileStream -> 'a) -> Async<'a>
+    // both two can throw exception
+
+
+// InMemory directory design
+(*
+    "root"
      └─(Exam id) folders
        ├─"Questions" folder
        │  └─(Question id) files
@@ -12,61 +33,56 @@ open System.IO
           └─(Submission id) files
 *)
 
-// what to do if the file already exist?
-// currently: just overlap
-type IFileSaver =
-    abstract SaveQuestion:
-        examId: Guid
-        -> questionId: Guid
-        -> questionStream: FileStream
-        -> Async<unit>
+let private saveFile directory filename (sourceStream: FileStream) = async {
+    if Directory.Exists directory |> not
+    then Directory.CreateDirectory directory |> ignore
 
-    abstract SaveSubmission:
-        examId: Guid
-        -> studentId: Guid
-        -> submissionId: Guid
-        -> submissionStream: FileStream
-        -> Async<unit>
-
-    // both two can throw exception
-
-type IFileGetter =
-    abstract GetQuestion: examId: Guid -> questionId: Guid -> Async<FileStream>
-    abstract GetSubmission: examId: Guid -> studentId: Guid -> submissionId: Guid -> Async<FileStream>
-
-
-// FIXME : indentation (readability)
-let private saveQuestion path
-    (examId: Guid)
-    (questionId: Guid)
-    (questionStream: FileStream) = async {
-    let questionPath = path + """\""" + examId.ToString() + """\Questions"""
-
-    if Directory.Exists questionPath |> not
-    then Directory.CreateDirectory questionPath |> ignore
-
-    use stream = File.Create (questionPath + """\""" + questionId.ToString())
-    return! questionStream.CopyToAsync stream |> Async.AwaitTask
+    use destStream = File.Create (directory + """\""" + filename)
+    return! sourceStream.CopyToAsync destStream |> Async.AwaitTask
 }
 
-let private saveSubmission path
-    (examId: Guid)
-    (studentId: Guid)
-    (submissionId: Guid)
-    (submissionStream: FileStream) = async {
-    let studentPath = path + """\""" + examId.ToString() + """\""" + studentId.ToString()
+let private getFile filePath callback = async {
+    use stream = File.OpenRead filePath
+    return callback stream
+}
 
-    if Directory.Exists studentPath |> not
-    then Directory.CreateDirectory studentPath |> ignore
+let private saveQuestion root (examId: Guid) (questionId: Guid) (questionStream: FileStream) = async {
+    let questionDirectory = root + """\""" + examId.ToString()
+                                 + """\Questions"""
+    return! saveFile questionDirectory (questionId.ToString()) questionStream
+}
 
-    use stream = File.Create (studentPath + """\""" + submissionId.ToString())
-    return! submissionStream.CopyToAsync stream |> Async.AwaitTask
+let private saveSubmission root (examId: Guid) (studentId: Guid) (submissionId: Guid) (submissionStream: FileStream) = async {
+    let studentDirectory = root + """\""" + examId.ToString()
+                                + """\""" + studentId.ToString()
+    return! saveFile studentDirectory (submissionId.ToString()) submissionStream
+}
+
+let private getQuestion root (examId: Guid) (questionId: Guid) callback = async {
+    let questionFilePath = root + """\""" + examId.ToString()
+                                + """\Questions"""
+                                + """\""" + questionId.ToString()
+    return! getFile questionFilePath callback
+}
+
+let private getSubmission root (examId: Guid) (studentId: Guid) (submissionId: Guid) callback = async {
+    let submissionFilePath = root + """\""" + examId.ToString()
+                                  + """\""" + studentId.ToString()
+                                  + """\""" + submissionId.ToString()
+    return! getFile submissionFilePath callback
 }
 
 
-let inMemoryFileSaver path =
+let inMemoryFileSaver root =
     { new IFileSaver with
         member _.SaveQuestion examId questionId questionStream
-            = saveQuestion path examId questionId questionStream
+            = saveQuestion root examId questionId questionStream
         member _.SaveSubmission examId studentId submissionId submissionStream
-            = saveSubmission path examId studentId submissionId submissionStream }
+            = saveSubmission root examId studentId submissionId submissionStream }
+
+let inMemoryFileGetter root =
+    { new IFileGetter with
+        member _.GetQuestion examId questionId callback
+            = getQuestion root examId questionId callback
+        member _.GetSubmission examId studentId submissionId callback
+            = getSubmission root examId studentId submissionId callback }
