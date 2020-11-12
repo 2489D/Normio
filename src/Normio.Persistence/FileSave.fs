af4feb5c356de08d19b6aaffa7d642ff6489fe69
SunghyunKimKAIST <harryzzong@kaist.ac.kr>
2020-11-12 22:57:55 +0900

    fix : file saver can throw exceptions

--- src/Normio.Persistence/FileSave.fs
+++ src/Normio.Persistence/FileSave.fs
@@ -17,52 +17,31 @@ type IFileSaver =
     // currently: just overlap
     abstract SaveQuestion: examId: Guid -> qId: Guid -> question: FileStream -> Async<unit>
     abstract SaveSubmission: examId: Guid -> sId: Guid -> submission: FileStream -> Async<unit>
+    // both two can throw exception
 
 
-type ValidDirectory = private ValidDirectory of string
-    with
-        member this.Value = this |> fun (ValidDirectory path) -> path
+type private InMemoryFileSaver(path) =
+    do Directory.CreateDirectory path |> ignore // can throw exception
 
-module ValidDirectory =
-    let create path =
-        try
-            Directory.CreateDirectory path |> ignore
-            path |> Some
-        with
-        | _ -> None // TODO
-        // docs.microsoft.com/en-us/dotnet/api/system.io.directory.createdirectory
-
-
-type private InMemoryFileSaver(path: ValidDirectory) =
     interface IFileSaver with
         member _.SaveQuestion eid qid q = async {
-            let qpath = path.Value + """\""" + eid.ToString() + """\Questions"""
+            let qpath = path + """\""" + eid.ToString() + """\Questions"""
 
             if Directory.Exists qpath |> not
             then Directory.CreateDirectory qpath |> ignore
 
-            try
-                use stream = File.Create (qpath + """\""" + qid.ToString())
-                return! q.CopyToAsync stream |> Async.AwaitTask
-            with
-            | _ -> () // TODO
-            // docs.microsoft.com/ko-kr/dotnet/api/system.io.file.create
-            // docs.microsoft.com/ko-kr/dotnet/api/system.io.stream.copytoasync
+            use stream = File.Create (qpath + """\""" + qid.ToString())
+            return! q.CopyToAsync stream |> Async.AwaitTask
         }
 
         member _.SaveSubmission eid sid s = async {
-            let spath = path.Value + """\""" + eid.ToString() + """\Submissions"""
+            let spath = path + """\""" + eid.ToString() + """\Submissions"""
 
             if Directory.Exists spath |> not
             then Directory.CreateDirectory spath |> ignore
 
-            try
-                use stream = File.Create (spath + """\""" + sid.ToString())
-                return! s.CopyToAsync stream |> Async.AwaitTask
-            with
-            | _ -> () // TODO
-            // docs.microsoft.com/ko-kr/dotnet/api/system.io.file.create
-            // docs.microsoft.com/ko-kr/dotnet/api/system.io.stream.copytoasync
+            use stream = File.Create (spath + """\""" + sid.ToString())
+            return! s.CopyToAsync stream |> Async.AwaitTask
         }
 
 let createInMemoryFileSaver path = (InMemoryFileSaver path) :> IFileSaver
