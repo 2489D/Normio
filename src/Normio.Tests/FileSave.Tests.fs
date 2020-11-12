46ee4ee835d418c95eb0a654c5fd9b9f77c7934e
SunghyunKimKAIST <harryzzong@kaist.ac.kr>
2020-11-12 23:24:24 +0900

    feat : file save test (not completed)

--- src/Normio.Tests/FileSave.Tests.fs
+++ src/Normio.Tests/FileSave.Tests.fs
@@ -1,8 +1,22 @@
 ﻿module Normio.Tests.FileSave_Tests
 
+open System
+open Xunit
+open Xunit.Sdk
+open FsUnit
+open FsUnit.Xunit
+
+open Normio.Persistence.FileSave
+
 /// File Saver Functionality
 /// 1. path -> directory validation
-/// 2. question
+/// 2. exam id, question id, question -> save question -> saves question
+/// 3. exam id, submission id, submission -> save submission -> saves submission
 
 [<Fact>]
-let ``file should be saved``
+[<Trait("Category", "FileSave")>]
+let ``file saver should be created``() =
+    try
+        createInMemoryFileSaver """\aejofnwl"""
+    with
+    | _ -> failwith "This should not fail"
